0ebfa4248a2d57ceca08d43228218e576f84aca5
Rick Copeland <rick@arborian.com>
2021-03-23 16:39:35 +0000

    Refactor with a blueprint

--- sls/app.py
+++ sls/app.py
@@ -1,17 +1,6 @@
-import os
-import hmac
-import struct
-import base64
-from datetime import datetime, timedelta
-from heapq import heappush, heappop
-
-from flask import Flask, request, abort, render_template
+from flask import Flask
 
 from . import bp
 
 app = Flask(__name__)
 app.register_blueprint(bp.mod)
-
-SECRETS = {}
-# Priority queue of secrets-to-expire
-EXPIRE = []
